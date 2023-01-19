9871ad0595b245cea0feb987b329c2dee5abc434
Ivan Novakov <inovakov.bg@gmail.com>
2023-01-19 15:23:30 +0200

    Add
    add list

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,23 @@
 import "../scss/app.scss";
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=10';
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
-
   const ul = document.querySelector("ul");
+
+  fetch(url).then((Response) => {
+
+    if (Response.status !== 200) {
+      console.log("Oh nooo, ${response status}");
+      return;
+    }
+
+    Response.json().then((data) => {
+      const arrayOfPok = data.results
+      arrayOfPok.map(pokname => {
+        const li = document.createElement("li");
+        li.innerText = pokname.name;
+        ul.appendChild(li);
+      });
+    });
+  });
 });
